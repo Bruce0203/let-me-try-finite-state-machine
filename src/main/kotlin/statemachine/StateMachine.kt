77294f84216c22248c6353b242a14f46cc1da87b
Bruce0203 <BruceFreedy@gmail.com>
2022-09-29 01:36:05 +0900

    fix(refector): combine class files

--- src/main/kotlin/statemachine/StateMachine.kt
+++ src/main/kotlin/statemachine/StateMachine.kt
@@ -4,6 +4,32 @@ class Event(var name: String)
 
 class Command(var name: String, var code: () -> Unit)
 
+class Controller(startName: String) {
+
+    private var currentState = State(startName)
+    internal val machine = StateMachine(currentState)
+
+    internal fun handle(target: State) {
+        if (currentState.hasTransition(target)) {
+            transitionTo(target)
+        }
+    }
+
+    internal fun call(transition: Transition) {
+        if (currentState.transitions.contains(transition)) {
+            transitionTo(transition.target)
+        }
+    }
+
+    private fun transitionTo(target: State): State {
+        currentState = target
+        target.executeActions()
+        return target
+    }
+
+
+}
+
 class StateMachine constructor(val start: State) {
 
     fun getStates(): HashSet<State> {
